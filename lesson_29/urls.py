1d85e3097edd9eeccf4fb3151a955759d7222025
sacred70 <sacred70@mail.ru>
2023-05-22 20:36:40 +0700

    1

--- lesson_29/urls.py
+++ lesson_29/urls.py
@@ -24,7 +24,8 @@ urlpatterns = [
     path('', root),
     path('admin/', admin.site.urls),
     path('cat/', include("ads.urls.cat")),
-    path('ad/', include("ads.urls.cat")),
+    path('ad/', include("ads.urls.ad")),
+    path('selection/', include("ads.urls.selection")),
     path('user/', include("users.urls.user")),
     path('location/', include("users.urls.location"))
 
--- users/urls/user.py
+++ users/urls/user.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from views.views import TokenObtainPairView, TokenRefreshView
 
 from users.views import *
 urlpatterns = [
@@ -7,5 +8,7 @@ urlpatterns = [
     path('create/', UserCreateView.as_view()),
     path('<int:pk>/update/', UserUpdateView.as_view()),
     path('<int:pk>/delete/', UserDeleteView.as_view()),
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh', TokenRefreshView.as_view()),
 
 ]
