37ac227b8cc9f662c4b18f629b487708fbc6661b
sacred70 <sacred70@mail.ru>
2023-05-22 20:32:23 +0700

    1

--- ads/serializers.py
+++ ads/serializers.py
@@ -3,7 +3,7 @@ import datetime
 from rest_framework.fields import SerializerMethodField
 from rest_framework.relations import SlugRelatedField
 from rest_framework.serializers import ModelSerializer
-from ads.models import Ad, Category
+from ads.models import Ad, Category, Selection
 from users.models import User
 from users.serializers import LocationSerializer
 
@@ -24,7 +24,12 @@ class AdSerializer(ModelSerializer):
     class Meta:
         model = Ad
         fields = '__all__'
-
+class AdCreateSerializer(ModelSerializer):
+    category = SlugRelatedField(slug_field="name", many=True, queryset=Category.objects.all())
+    author = SlugRelatedField(slug_field="username", many=True, queryset=User.objects.all())
+    class Meta:
+        model = Ad
+        fields = '__all__'
 
 class AdListSerializer(ModelSerializer):
     category = SlugRelatedField(slug_field="name", many=True, queryset=Category.objects.all())
@@ -44,4 +49,24 @@ class AdDetailSerializer(ModelSerializer):
 
     class Meta:
         model = Ad
+        fields = '__all__'
+
+
+class SelectionSerializer(ModelSerializer):
+
+    class Meta:
+        model = Selection
+        fields = '__all__'
+
+
+class SelectionCreateSerializer(ModelSerializer):
+    owner = SlugRelatedField(slug_field="username", read_only=True)
+    def create(self, validated_data):
+        request = self.context.get("request")
+        validated_data["owner"] = request.user
+        return super().create(validated_data)
+
+
+    class Meta:
+        model = Selection
         fields = '__all__'
