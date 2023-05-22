1623aa48a9baf2b20b6bf1127862a481e40ff159
sacred70 <sacred70@mail.ru>
2023-05-22 20:31:30 +0700

    1

--- ads/models.py
+++ ads/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import UniqueConstraint
 
 from users.models import User
 
@@ -19,18 +20,19 @@ class Ad(models.Model):
     def __str__(self):
         return self.name
 
-    def serialize(self):
-        return {
-        'id' : self.id,
-        'name' : self.name,
-        'author' : self.author.username,
-        'price' : self.price,
-        'description' : self.description,
-        'category': self.category,
-        'image' : self.image.url if self.image else None,
-        'is_published' : self.is_published
-        }
 
+class Selection(models.Model):
+    name = models.CharField(max_length=200)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField(Ad)
+    constraints = [UniqueConstraint(fields=["name", "owner"], name="my_constraint")]
+    class Meta:
+        verbose_name = "Подборка"
+        verbose_name_plural = "Подборки"
+
+
+    def __str__(self):
+        return self.name
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
